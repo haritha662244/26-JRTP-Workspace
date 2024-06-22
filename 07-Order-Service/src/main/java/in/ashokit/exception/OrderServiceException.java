586f8405de25cc52f63dbf37765de8cabe85f8ea
haritha <gunjiharitha2001@gmail.com>
2024-06-22 22:20:34 +0530

    07-Order-Service

---
+++ 07-Order-Service/src/main/java/in/ashokit/exception/OrderServiceException.java
@@ -0,0 +1,27 @@
+
+package in.ashokit.exception;
+
+import lombok.Builder;
+
+
+public class OrderServiceException extends RuntimeException {
+	private String errCode;
+
+	public OrderServiceException() {
+
+	}
+
+	public OrderServiceException(String msg, String errCode) {
+		super(msg);
+		this.errCode = errCode;
+	}
+
+	public String getErrCode() {
+		return errCode;
+	}
+
+	public void setErrCode(String errCode) {
+		this.errCode = errCode;
+	}
+	
+}
